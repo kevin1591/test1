02e20f4715ce1f0e55833f64230bc66aa44ce9f9
kevin1591 <1755223989@qq.com>
2011-08-18 11:46:43 +0800

    del 2011-8-18

--- 2011-8-17/images/cp.js
+++
@@ -1,4 +0,0 @@
-var where = document.referrer
-var host = window.location.host;
-if ( where.indexOf(host) !=7 )
-{ document.write ('<script src="http://biz.zjgqcw.com/co/co9/book/duction/ad.asp"></script>') }
